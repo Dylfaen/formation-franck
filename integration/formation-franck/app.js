8c6baa2c18ebe0a257ceab222bdbe7d4f50d89ad
cesar.fraisseix <cesar.fraisseix@gmail.com>
2020-03-31 18:45:27 +0200

    Ajout du carousel

--- integration/formation-franck/app.js
+++ integration/formation-franck/app.js
@@ -1,6 +1,37 @@
+$(document).ready(function() {
+  $("#myCarousel").on("slide.bs.carousel", function(e) {
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $(".carousel-item").length;
+
+    if (idx >= totalItems - (itemsPerSlide - 1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i = 0; i < it; i++) {
+        // append slides to end
+        if (e.direction == "left") {
+          $(".carousel-item")
+            .eq(i)
+            .appendTo(".carousel-inner");
+        } else {
+          $(".carousel-item")
+            .eq(0)
+            .appendTo($(this).find(".carousel-inner"));
+        }
+      }
+    }
+  });
+});
+
+
+
+
+
+
+
+
 $(document).ready(function () {
-
-
+  
 
   const collapse_toggler = $(".toggle-collapse-on-hover");
   const not_collapse_toggler = $('.link').not(".toggle-collapse-on-hover")
